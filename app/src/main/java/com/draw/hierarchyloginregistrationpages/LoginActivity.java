1ea4d143469ff662ac0bfa95f940924e81d7bc1e
Karthik Ganiga <karthikganiga007@gmail.com>
2019-07-07 01:22:28 +0530

    registration file

---
+++ app/src/main/java/com/draw/hierarchyloginregistrationpages/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.draw.hierarchyloginregistrationpages;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        register = (TextView) findViewById(R.id.register);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/draw/hierarchyloginregistrationpages/RegisterActivity.java
@@ -0,0 +1,12 @@
+package com.draw.hierarchyloginregistrationpages;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration);
+    }
+}
